86d5a98c8f9fefc1e94e7c0ec4f0ecd18d475f00
ver2point0 <jmci10@hotmail.com>
2021-06-27 23:37:22 -0700

    update queries

--- database/queries.sql
+++ database/queries.sql
@@ -1,1 +1,12 @@
 -- Queries go here
+
+-- Retrieve the first name, last name, and email address of all customers that have a Gmail email address.
+
+
+-- Retrieve the address of the customers and the order IDs for all orders that were placed in 2020
+
+
+-- Retrieve all product details for products that are under the "Kitchen" category
+
+
+-- Retrieve the product names and prices of all products ordered by the customer with first name "Bugs" and last name "Bunny"
